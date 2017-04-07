3d7499c1824517ff3cc1d5a57817b53a3442fbd5
Chrispine Otaalo <john.otaalo@strathmore.edu>
2017-04-07 18:45:43 +0300

    Made a few changes to the template

--- application/modules/Template/views/frontend_template_v_2.php
+++ application/modules/Template/views/frontend_template_v_2.php
@@ -38,206 +38,109 @@
 <style type="text/css">.custom-css-1488241484908{padding-top:90px;padding-bottom:90px}.custom-css-1488239830737{background-color:#fff;background-color:#fff}.custom-css-1488241938953{background-color:#28353d}.custom-css-1488244328363{padding:90px 20px 20px}.custom-css-1488245476729{padding-top:50px;padding-bottom:50px;background-color:#fff}.custom-css-1488041420143{background-color:#28353d;padding:80px 12%}.custom-css-1488041425297{padding:80px 12%}.custom-css-1488041385976{padding:80px 12%}.custom-css-1488239923758{border-color:#eee;border-style:solid;border-width:10px;padding:80px 12%}.custom-css-1488241686986{padding-top:200px}.custom-css-1488241607219{padding-top:200px}.custom-css-1488239923758{border-color:#eee;border-style:solid;border-width:10px;padding:80px 12%}.custom-css-1488242558389{padding:80px 12%}.custom-css-1488243026145{padding-top:200px}.custom-css-1488243053150{padding-top:200px}.custom-css-1488242590341{padding:80px 12%}.custom-css-1488245539123{background-color:rgba(255,255,255,0.01);*background-color:#fff;padding:0}</style>
 </head>
 <body class="header-transparent no-page-heading fluid-width no-top-content-padding no-bottom-content-padding">
-<div id="page" class="site">
-<header id="masthead" class="site-header header-resize">
-<!-- <div class="header-cart">
-<div class="mini-cart">
-<div class="mini-cart-content">
-<ul class="mini-cart-list">
-<li class="mini-cart-item">
-<a href="#" class="remove">&times;</a>
-<a class="mini-cart-link" href="#">
-<div class="image-thumbnail thumbnail-size">
-<img src="data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" class="mini-cart-image lazy-load-img" alt="photo-1436367050586-7c605120bf73" data-src="https://demo.apalodi.com/azra/wp-content/uploads/sites/2/2017/01/photo-1436367050586-7c605120bf73-150x150.jpg">
-</div>
-<div class="mini-cart-info">
-<h4 class="mini-cart-title">Blue Suit</h4>
-<span class="quantity">1 &times; <span class="price-amount"><span class="price-currency">&#36;</span>799.00</span></span>
-</div>
-</a>
-</li>
-<li class="mini-cart-item">
-<a href="#" class="remove">&times;</a>
-<a class="mini-cart-link" href="#">
-<div class="image-thumbnail thumbnail-size">
-<img src="data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" class="mini-cart-image lazy-load-img" alt="Oldtimer" data-src="https://demo.apalodi.com/azra/wp-content/uploads/sites/2/2017/01/photo-1464219789935-c2d9d9aba644-150x150.jpg">
-</div>
-<div class="mini-cart-info">
-<h4 class="mini-cart-title">Old Timers</h4>
-<span class="quantity">1 &times; <span class="price-amount"><span class="price-currency">&#36;</span>899.00</span></span>
-</div>
-</a>
-</li>
-<li class="mini-cart-item">
-<a href="#" class="remove">&times;</a>
-<a class="mini-cart-link" href="shop/product.html">
-<div class="image-thumbnail thumbnail-size">
-<img src="data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" class="mini-cart-image lazy-load-img" alt="Headphones" data-src="https://demo.apalodi.com/azra/wp-content/uploads/sites/2/2017/01/e-aupyxbjm-alice-moore-150x150.jpg">
-</div>
-<div class="mini-cart-info">
-<h4 class="mini-cart-title">Headphones</h4>
-<span class="quantity">1 &times; <span class="price-amount"><span class="price-currency">&#36;</span>99.00</span></span>
-</div>
-</a>
-</li>
-</ul>
-</div>
-<div class="mini-cart-meta">
-<p class="mini-cart-total">
-<span class="mini-cart-subtotal">Subtotal:</span>
-<span class="mini-cart-subtotal-amount"><span class="price-amount"><span class="price-currency">&#36;</span>1,797.00</span></span>
-</p>
-<div class="mini-cart-actions">
-<a href="shop/cart.html" class="button button-cart"><i class="apalodi-icon-cart-7"></i><span>View Cart</span></a>
-<a href="shop/checkout.html" class="button button-checkout"><i class="apalodi-icon-right-circled-alt"></i><span>Checkout</span></a>
-</div>
-</div>
-</div>
-</div> -->
+	<div id="page" class="site">
+		<header id="masthead" class="site-header header-resize is-light is-transparent">
+			<div class="site-header-container container">
+			<div class="logo">
+				<a href="<?= @base_url('Home/');?>" rel="home">
+					<img class="logo-default" src="<?= @$assets_url; ?>img/ministry-of-health.png" alt="Ministry-of-Health">
+				</a>
+			</div>
+			<div class="site-header-inner">
+				<div class="header-search-form">
+					<form role="search" method="get" class="search-form" action="/">
+						<label>
+							<span class="screen-reader-text">Search for:</span>
+							<input type="search" class="search-field" placeholder="Search" value="" name="s"/>
+						</label>
+						<button type="submit" class="search-submit"><span>Search</span></button>
+					</form>
+					<span class="header-search-form-close site-action-close"></span>
+				</div>
+				<div class="site-navigation-wrapper">
+					<nav class="main-navigation">
 
-<div class="logo">
-<a href="<?= @base_url('Home/');?>" rel="home">
-<img class="logo-default" src="<?= @$assets_url; ?>img/lvct.jpg" alt="LVCT">
-</a>
-</div>
+					<ul class="site-nav">
+						<li class="menu-item menu-item-has-children menu-no-link mega-menu mm-col-2 mega-menu-center"><a href="<?= @base_url('Home/');?>"><span><span class="menu-item-label">Home</span></span></a>
+						</li>
+
+						<li class="menu-item menu-item-has-children menu-no-link"><a href="<?= @base_url('Home/FAQ/');?>"><span><span class="menu-item-label">FAQs</span></span></a>
+						</li>
 
 
+						<li class="menu-item menu-item-has-children menu-no-link"><a href="<?= @base_url('Home/videos/');?>"><span><span class="menu-item-label">Conduct a Test</span></span></a>
+						</li>
 
-<div class="site-header-container container">
-<!-- <div class="logo">
-<a href="<?= @base_url('Home/');?>" rel="home">
-<img class="logo-default" src="<?= @$assets_url; ?>img/one.jpg" alt="One2One">
-</a>
-</div> -->
+						
 
-<div class="logo">
-<a href="<?= @base_url('Home/');?>" rel="home">
-<img class="logo-default" src="<?= @$assets_url; ?>img/ministry-of-health.png" alt="Ministry-of-Health">
-</a>
-</div>
+						<li class="menu-item menu-item-has-children menu-no-link"><a href="<?= @base_url('Outlets'); ?>"><span><span class="menu-item-label">Get a Kit</span></span></a>
+						</li>
 
+						<li class="menu-item menu-item-has-children menu-no-link"><a href="<?= @base_url('Map'); ?>"><span><span class="menu-item-label">Health Professionals</span></span></a>
+						</li>
 
+						<li class="menu-item menu-item-has-children menu-no-link"><a href="<?= @base_url('Home/Resources/');?>"><span><span class="menu-item-label">Resources</span></span></a>
+						</li>
+						
 
+						<li class="menu-item menu-font-icon menu-button"><a target="_blank" href="#"><span><span class="menu-item-label">Login</span></span></a></li>
+					</ul>
 
-
-
-
-<!-- <div class="site-actions">
-<span class="header-cart-trigger site-action-trigger apalodi-icon-cart-7"> <span class="header-cart-count site-action-info">3</span></span>
-<span class="header-cart-close-trigger site-action-close slide-out-action-close"><span></span></span>
-<span class="header-search-trigger site-action-trigger apalodi-icon-search-alt"><span></span></span>
-<span class="mobile-menu-trigger hamburger-menu"><span></span></span>
-<span class="mobile-menu-close-trigger site-action-close slide-out-action-close"><span></span></span>
-<span class="site-actions-overlay"></span>
-</div> -->
-<div class="site-header-inner">
-<div class="header-search-form">
-<form role="search" method="get" class="search-form" action="/">
-<label>
-<span class="screen-reader-text">Search for:</span>
-<input type="search" class="search-field" placeholder="Search" value="" name="s"/>
-</label>
-<button type="submit" class="search-submit"><span>Search</span></button>
-</form>
-<span class="header-search-form-close site-action-close"></span>
-</div>
-<div class="site-navigation-wrapper">
-<nav class="main-navigation">
-
-<ul class="site-nav">
-	<li class="menu-item menu-item-has-children menu-no-link mega-menu mm-col-2 mega-menu-center"><a href="<?= @base_url('Home/');?>"><span><span class="menu-item-label">Home</span></span></a>
-	</li>
-
-	<li class="menu-item menu-item-has-children menu-no-link"><a href="<?= @base_url('Home/FAQ/');?>"><span><span class="menu-item-label">FAQs</span></span></a>
-	</li>
+					</nav>
+				</div>
+			</div>
+			</div>
+		</header>
 
 
-	<li class="menu-item menu-item-has-children menu-no-link"><a href="<?= @base_url('Home/videos/');?>"><span><span class="menu-item-label">Conduct a Test</span></span></a>
-	</li>
-
-	
-
-	<li class="menu-item menu-item-has-children menu-no-link"><a href="<?= @base_url('Outlets'); ?>"><span><span class="menu-item-label">Get a Kit</span></span></a>
-	</li>
-
-	<li class="menu-item menu-item-has-children menu-no-link"><a href="<?= @base_url('Map'); ?>"><span><span class="menu-item-label">Health Professionals</span></span></a>
-	</li>
-
-	<li class="menu-item menu-item-has-children menu-no-link"><a href="<?= @base_url('Home/Resources/');?>"><span><span class="menu-item-label">Resources</span></span></a>
-	</li>
-
-	<!-- <li class="menu-item menu-item-has-children menu-no-link"><a><span><span class="menu-item-label">Conduct a Test</span></span></a>
-		<ul class="sub-menu">
-		<li class="menu-item"><a href="<?= @base_url('Home/videos/');?>"><span><span class="menu-item-label">Instructional Videos</span></span></a></li>
-		<li class="menu-item menu-description"><a href="#"><span><span class="menu-item-label">Upload IFU</span></span></a></li>
-		</ul>
-	</li> -->
-	
-
-	<li class="menu-item menu-font-icon menu-button"><a target="_blank" href="#"><span><span class="menu-item-label">Login</span></span></a></li>
-</ul>
-
-</nav>
-</div>
-</div>
-</div>
-</header>
+		<?= $this->load->view($partial, $partialData); ?>
 
 
-<?= $this->load->view($partial, $partialData); ?>
+		<footer id="colophon" class="site-footer">
+			<div class="site-footer-container container">
+				<div class="footer-logo">
+					<a href="index.html" rel="home">
+						<img src="<?= @$assets_url; ?>img/logo-footer.png" alt="Azra">
+					</a>
+				</div>
+				<div class="footer-content">
+					<nav class="footer-menu">
+						<ul>
+						<li class="menu-item"><a href="about-us.html">Do you need information, counseling or someone friendly to talk to about HIV & AIDS, Sex, STIs, Contraceptives, sexual and Gender Based Violence and other health, Teen or Youth related issues? CALL/SMS: 1190 Free from Safaricom line (Everyday 8am-8pm )</a></li>
+						</ul>
+					</nav>
+					<div class="copyright">
+						<p>© <a href="http://www.one2onekenya.org" target="_blank">One 2 One</a> and <a href="" target="_blank">LVCTHealth</a></p>
+					</div>
+				</div>
+				<div class="footer-social">
+					<div class="social-icons">
+						<a class="social-icon icon-facebook" href="https://www.facebook.com/One2one-ke-144444975612410/?hc_location=ufi" target="_blank"><span>facebook</span></a>
+
+						<a class="social-icon icon-twitter" href="https://twitter.com/one2oneke?lang=en" target="_blank"><span>twitter</span></a>
+						<a class="social-icon icon-instagram" href="https://www.instagram.com/onetooneke/?hl=en" target="_blank"><span>instagram</span></a>
+
+						<i class="social social-whatsapp x1.5"></i> 0700-121-121
+
+					</div>
+				</div>
+			</div>
+		</footer>
 
 
-<footer id="colophon" class="site-footer">
-<div class="site-footer-container container">
-<div class="footer-logo">
-<a href="index.html" rel="home">
-<img src="<?= @$assets_url; ?>img/logo-footer.png" alt="Azra">
-</a>
-</div>
-<div class="footer-content">
-<nav class="footer-menu">
-<ul>
-<li class="menu-item"><a href="about-us.html">Do you need information, counseling or someone friendly to talk to about HIV & AIDS, Sex, STIs, Contraceptives, sexual and Gender Based Violence and other health, Teen or Youth related issues? CALL/SMS: 1190 Free from Safaricom line (Everyday 8am-8pm )</a></li>
-<!-- <li class="menu-item"><a href="our-process.html">Terms of Use</a></li>
-<li class="menu-item"><a href="contact-us-2.html">Contact</a></li>
-<li class="menu-item"><a href="our-team.html">Carrers</a></li>
-<li class="menu-item"><a href="our-services.html">Services</a></li> -->
-</ul>
-</nav>
-<div class="copyright">
-<p>© <a href="http://www.one2onekenya.org" target="_blank">One 2 One</a> and <a href="" target="_blank">LVCTHealth</a></p>
-</div>
-</div>
-<div class="footer-social">
-<div class="social-icons">
-<a class="social-icon icon-facebook" href="https://www.facebook.com/One2one-ke-144444975612410/?hc_location=ufi" target="_blank"><span>facebook</span></a>
-
-<a class="social-icon icon-twitter" href="https://twitter.com/one2oneke?lang=en" target="_blank"><span>twitter</span></a>
-<a class="social-icon icon-instagram" href="https://www.instagram.com/onetooneke/?hl=en" target="_blank"><span>instagram</span></a>
-
-<i class="social social-whatsapp x1.5"> 0700-121-121</i>
-
-</div>
-</div>
-</div>
-</footer>
+	</div>
 
 
-</div>
+	<button class="button back-to-top"></button>
 
+	<link rel="stylesheet" href="<?= @$assets_url; ?>css/animate.min.css" type="text/css" media="all">
+	<script type="text/javascript" src="<?= @$assets_url; ?>js/plugins.js"></script>
+	<script type="text/javascript" src="<?= @$assets_url; ?>js/main.js"></script>
 
-<button class="button back-to-top"></button>
-
-<link rel="stylesheet" href="<?= @$assets_url; ?>css/animate.min.css" type="text/css" media="all">
-<script type="text/javascript" src="<?= @$assets_url; ?>js/plugins.js"></script>
-<script type="text/javascript" src="<?= @$assets_url; ?>js/main.js"></script>
-
-<script type="text/javascript" src="<?= @$assets_url; ?>js/vendor/jquery.swiper.min.js"></script>
-<script type="text/javascript" src="<?= @$assets_url; ?>js/vendor/aslider.js"></script>
-<script type="text/javascript" src="<?= @$assets_url; ?>js/vendor/viba-portfolio.js"></script>
-<script type="text/javascript" src="<?= @$assets_url; ?>js/vendor/owl.carousel.min.js"></script>
-<script type="text/javascript" src="<?= @$assets_url; ?>js/vendor/isotope.pkgd.min.js"></script>
-
-
-
-</body></html>
+	<script type="text/javascript" src="<?= @$assets_url; ?>js/vendor/jquery.swiper.min.js"></script>
+	<script type="text/javascript" src="<?= @$assets_url; ?>js/vendor/aslider.js"></script>
+	<script type="text/javascript" src="<?= @$assets_url; ?>js/vendor/viba-portfolio.js"></script>
+	<script type="text/javascript" src="<?= @$assets_url; ?>js/vendor/owl.carousel.min.js"></script>
+	<script type="text/javascript" src="<?= @$assets_url; ?>js/vendor/isotope.pkgd.min.js"></script>
+</body>
+</html>
