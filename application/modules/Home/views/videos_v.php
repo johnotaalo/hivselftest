58960874277835f553d66cd2a2eec7bf2796ad22
Chrispine Otaalo <john.otaalo@strathmore.edu>
2017-04-09 16:06:40 +0300

    Added the videos to YouTube and embedded them onto the site

--- application/modules/Home/views/videos_v.php
+++ application/modules/Home/views/videos_v.php
@@ -1,48 +1,59 @@
 
 <div id="page-heading" class="site-heading is-light site-heading-align-center">
-<div class="site-heading-container container skrollable skrollable-between" data-start="transform: translate3d(0,0%,0); opacity:1" data-top-bottom="transform: translate3d(0,65%,0); opacity:0" style="transform: translate3d(0px, 0%, 0px); opacity: 1;">
-<div class="site-heading-inner">
-<h1 class="site-title">Videos</h1>
-</div>
-</div>
-</div>
-<div id="content" class="site-content container">
-<div id="primary" class="content-area">
-<main id="main" class="site-main">
-<div class="content">
-<div class="row-wrapper">
-<div class="row-container container">
-<div class="row">
-<div class="column col-sm-6">
-<div class="column-inner">
-<div class="column-content">
-<div class="content-element video-wrapper">
-
-<div class="iframe-wrapper iframe-video"><iframe src="https://www.youtube.com/embed/N4CAqsmN_6g" frameborder="0" allowfullscreen></iframe></div>
-
-<!-- <div class="iframe-wrapper iframe-video"><iframe width="560" height="315" src="https://www.youtube.com/embed/aatr_2MstrI" frameborder="0" allowfullscreen></iframe></div> -->
-
-</div>
-</div>
-</div>
-</div>
-<div class="column col-sm-6">
-<div class="column-inner">
-<div class="column-content">
-<div class="content-element video-wrapper">
-<div class="iframe-wrapper iframe-video"><iframe src="https://www.youtube.com/embed/WdoMwzTguV0" frameborder="0" allowfullscreen></iframe></div>
-<!-- width="854" height="480" -->
-</div>
-</div>
-</div>
-</div>
-</div>
-</div>
+	<div class="site-heading-container container skrollable skrollable-between" data-start="transform: translate3d(0,0%,0); opacity:1" data-top-bottom="transform: translate3d(0,65%,0); opacity:0" style="transform: translate3d(0px, 0%, 0px); opacity: 1;">
+		<div class="site-heading-inner">
+			<h1 class="site-title">Videos</h1>
+		</div>
+	</div>
 </div>
 
-</div>
-</main>
-</div>
+<div id="content" class="site-content container" style="margin-top: 15px;">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+			<div class="content">
+				<div class="row-wrapper">
+					<div class="row-container container">
+						<div class="row">
+							<div class="column col-sm-4">
+								<div class="column-inner">
+									<div class="column-content">
+										<div class="content-element video-wrapper">
+											<div class="iframe-wrapper iframe-video">
+												<iframe width="560" height="315" src="https://www.youtube.com/embed/90s8raQPrhk" frameborder="0" allowfullscreen></iframe>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="column col-sm-4">
+								<div class="column-inner">
+									<div class="column-content">
+										<div class="content-element video-wrapper">
+											<div class="iframe-wrapper iframe-video">
+												<iframe width="560" height="315" src="https://www.youtube.com/embed/dlldduI79ic" frameborder="0" allowfullscreen></iframe>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="column col-sm-4">
+								<div class="column-inner">
+									<div class="column-content">
+										<div class="content-element video-wrapper">
+											<div class="iframe-wrapper iframe-video">
+												<iframe width="560" height="315" src="https://www.youtube.com/embed/vv1LXV-s138" frameborder="0" allowfullscreen></iframe>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</main>
+	</div>
 </div>
 
 
