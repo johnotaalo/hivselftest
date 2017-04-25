08f1c65a8e358b1b5439055d85dcac547dbc9a6a
WillyMareka <marekawilly@gmail.com>
2017-04-26 00:42:49 +0300

    Removed the others from gender

--- application/modules/Template/views/frontend_template_v_2.php
+++ application/modules/Template/views/frontend_template_v_2.php
@@ -164,7 +164,6 @@
 		  			<option value="">Select your gender</option>
 			  		<option value="Male">Male</option>
 			  		<option value="Female">Female</option>
-			  		<option value="Other">Other</option>
 				</select>
 			</div>
 
