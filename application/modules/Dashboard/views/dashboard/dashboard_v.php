36eff0203184b716270294f9503d234952954466
Chrispine Otaalo <john.otaalo@strathmore.edu>
2017-05-11 18:08:53 +0300

    Google Analytics in Dashboard

--- application/modules/Dashboard/views/dashboard/dashboard_v.php
+++ application/modules/Dashboard/views/dashboard/dashboard_v.php
@@ -1,4 +1,153 @@
-<div class="row">
+<style type="text/css">
+	select.FormField{
+		display: block;
+		width: 100%;
+		height: 34px;
+		padding: 6px 12px;
+		font-size: 14px;
+		line-height: 1.42857143;
+		color: #555;
+		background-color: #fff;
+		background-image: none;
+		border: 1px solid #ccc;
+		border-radius: 4px;
+		-webkit-box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075);
+		box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075);
+		-webkit-transition: border-color ease-in-out .15s, -webkit-box-shadow ease-in-out .15s;
+		-o-transition: border-color ease-in-out .15s, box-shadow ease-in-out .15s;
+		transition: border-color ease-in-out .15s, box-shadow ease-in-out .15s;
+	}
+
+	.Dashboard-footer, .Dashboard-header {
+		margin: -1.5em -1.5em 1.5em;
+		padding: 1.5em;
+	}
+
+	.Dashboard-header {
+		border-bottom: 1px solid #d4d2d0;
+	}
+
+	@media (min-width: 1024px){
+		.FlexGrid {
+			margin: 0 0 -2em -2em !important;
+		}
+	}
+
+	.FlexGrid {
+		display: -webkit-box;
+		display: -webkit-flex;
+		display: -ms-flexbox;
+		display: flex;
+		-webkit-flex-wrap: wrap;
+		-ms-flex-wrap: wrap;
+		flex-wrap: wrap;
+		list-style: none;
+		margin: 0 0 -1.5em -1.5em;
+		padding: 0;
+	}
+
+	@media (min-width: 1024px){
+		.FlexGrid-item {
+			margin: 0 0 2em 2em !important;
+		}
+	}
+
+	@media (min-width: 570px){
+		.FlexGrid-item {
+			-webkit-flex-basis: 200px !important;
+			-ms-flex-preferred-size: 200px !important;
+			flex-basis: 200px !important;
+		}
+	}
+
+	.FlexGrid-item {
+		-webkit-box-flex: 1;
+		-webkit-flex: 1 0 -webkit-calc(100% - 1.5em);
+		-ms-flex: 1 0 calc(100% - 1.5em);
+		flex: 1 0 calc(100% - 1.5em);
+		margin: 0 0 1.5em 1.5em;
+		float: left;
+	}
+
+	.FlexGrid-item--fixed {
+		-webkit-box-flex: 0!important;
+		-webkit-flex: 0 0 auto!important;
+		-ms-flex: 0 0 auto!important;
+		flex: 0 0 auto!important;
+	}
+
+	.Titles {
+		font-weight: 300;
+		line-height: 1.2;
+		margin: 0 0 1.5em;
+	}
+
+	.Titles-main {
+		font-size: 1.4em !important;
+	}
+
+	.Titles-main, .Titles-sub {
+		color: inherit;
+		font: inherit;
+		margin: 0;
+	}
+
+	.Titles-sub {
+		opacity: .6;
+		margin-top: .2em;
+	}
+	@media (min-width: 570px){
+		.ViewSelector, .ViewSelector2 {
+			display: -webkit-box;
+			display: -webkit-flex;
+			display: -ms-flexbox;
+			display: flex;
+			margin: 0 0 -1em -1em;
+			width: -webkit-calc(100% + 1em);
+			width: calc(100% + 1em);
+		}
+	}
+
+	@media (min-width: 570px){
+		.ViewSelector2-item, .ViewSelector table {
+			-webkit-box-flex: 1;
+			-webkit-flex: 1 1 -webkit-calc(100%/3 - 1em);
+			-ms-flex: 1 1 calc(100%/3 - 1em);
+			flex: 1 1 calc(100%/3 - 1em);
+			margin-left: 1em;
+		}
+	}
+
+	.ViewSelector2-item, .ViewSelector table {
+		display: block;
+		margin-bottom: 1em;
+		width: 100%;
+	}
+
+	.ViewSelector2-item>label, .ViewSelector td:first-child {
+		font-weight: 700;
+		margin: 0 .25em .25em 0;
+		display: block;
+	}
+</style>
+<script>
+(function(w,d,s,g,js,fs){
+	g=w.gapi||(w.gapi={});g.analytics={q:[],ready:function(f){this.q.push(f);}};
+	js=d.createElement(s);fs=d.getElementsByTagName(s)[0];
+	js.src='https://apis.google.com/js/platform.js';
+	fs.parentNode.insertBefore(js,fs);js.onload=function(){g.load('analytics');};
+}(window,document,'script'));
+
+gapi.analytics.ready(function() {
+	var CLIENT_ID = '<?= @$this->config->item('client_id'); ?>';
+
+	gapi.analytics.auth.authorize({
+		container: 'embed-api-auth-container',
+		clientid: CLIENT_ID,
+	});
+});
+</script>
+<!-- <div class="row">
 	<div class="col-lg-12">
 
 		<div class="hpanel">
@@ -22,16 +171,13 @@
 						</h1>
 						<small>Page views in <span class = "number_of_days">Last 7 Days</span></small>
 						</div>
-						<!-- <div class="small m-t-xl">
-							<i class="fa fa-clock-o"></i> <span class = "from_date">Data from January</span>
-						</div> -->
 					</div>
 					<div class="col-md-6">
 						<div class="text-center small">
-							<i class="fa fa-laptop"></i> Active users in current month (December)
+							<i class="fa fa-laptop"></i> Active users in current month (<?= @date('F'); ?>)
 						</div>
 						<div class="flot-chart" style="height: 160px">
-							<div class="flot-chart-content" id="flot-line-chart"></div>
+							
 						</div>
 					</div>
 					<div class="col-md-3 text-center">
@@ -44,21 +190,12 @@
 							</h1>
 							<small>Data From Google Analytics</small>
 						</div>
-						<!-- <div class="small m-t-xl">
-							<i class="fa fa-clock-o"></i> Last active in 12.10.2015
-						</div> -->
 					</div>
 				</div>
 			</div>
-			<!-- <div class="panel-footer">
-			<span class="pull-right">
-			You have two new messages from <a href="#">Monica Bolt</a>
-			</span>
-			Last update: 21.05.2015
-			</div> -->
 		</div>
 	</div>
-</div>
+</div> -->
 <div class="row">
 	<div class="col-lg-3">
 		<div class="hpanel">
@@ -150,5 +287,57 @@
 				</div>
 			</div>
 		</div>
+	</div>
+</div>
+
+<div class="row">
+	<div class="col-md-12">
+		<div class="hpanel">
+			<div class="panel-body h-200">
+				<header class = "Dashboard-header">
+					<ul class="FlexGrid">
+						<li class="FlexGrid-item">
+							<div class="Titles">
+								<h1 class="Titles-main" id="view-name"></h1>
+								<div class="Titles-sub">Various visualizations</div>
+							</div>
+						</li>
+						<li class="FlexGrid-item FlexGrid-item--fixed">
+							<div id="active-users-container"></div>
+						</li>
+					</ul>
+					<div id="view-selector-container"></div>
+				</header>
+				<div class="row">
+					<div class="col-md-6">
+						<h3>This Week vs Last Week (by sessions)</h3>
+						<figure class="Chartjs-figure" id="chart-1-container"></figure>
+						<ol class="Chartjs-legend" id="legend-1-container"></ol>
+					</div>
+					<div class="col-md-6">
+						<h3>This Year vs Last Year (by users)</h3>
+						<figure class="Chartjs-figure" id="chart-2-container"></figure>
+						<ol class="Chartjs-legend" id="legend-2-container"></ol>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-6">
+						<h3>Top Browsers (by pageview)</h3>
+						<figure class="Chartjs-figure" id="chart-3-container"></figure>
+						<ol class="Chartjs-legend" id="legend-3-container"></ol>
+					</div>
+					<div class="col-md-6">
+						<h3>Top Countries (by sessions)</h3>
+						<figure class="Chartjs-figure" id="chart-4-container"></figure>
+						<ol class="Chartjs-legend" id="legend-4-container"></ol>
+					</div>
+				</div>
+				
+			</div>
+
+			<div class="panel-footer">
+				<div id="embed-api-auth-container"></div>
+			</div>
+		</div>	
 	</div>
 </div>
