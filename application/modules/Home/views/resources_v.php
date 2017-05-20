c7e46167363916dfa91c54ee132d62a519afebaf
WillyMareka <marekawilly@gmail.com>
2017-05-20 17:22:35 +0300

    Updates on the resources page

--- application/modules/Home/views/resources_v.php
+++ application/modules/Home/views/resources_v.php
@@ -8,20 +8,20 @@
 </div>
 <div class="bg-media-overlay"></div>
 </div>
-<div class="site-heading-container container skrollable skrollable-between" data-start="transform: translate3d(0,0%,0); opacity:1" data-top-bottom="transform: translate3d(0,65%,0); opacity:0" style="transform: translate3d(0px, 0%, 0px); opacity: 1;">
-<div class="site-heading-inner">
-<h2 class="site-subtitle">HIV Self Testing</h2><h1 class="site-title">Resources</h1>
-</div>
-</div>
+	<div class="site-heading-container container skrollable skrollable-between" data-start="transform: translate3d(0,0%,0); opacity:1" data-top-bottom="transform: translate3d(0,65%,0); opacity:0" style="transform: translate3d(0px, 0%, 0px); opacity: 1;">
+	<div class="site-heading-inner">
+	<h2 class="site-subtitle">HIV Self Testing</h2><h1 class="site-title">Resources</h1>
+	</div>
+	</div>
 </div>
 
 <div style="margin-top:30px;" class="content-element separator">
-											<span class="separator-line separator-line-left"></span>
-											<div class="separator-content">
-												<h4 class="separator-title">Resources</h4>
-											</div>
-											<span class="separator-line separator-line-right"></span>
-										</div>
+	<span class="separator-line separator-line-left"></span>
+	<div class="separator-content">
+		<h4 class="separator-title"><b>Resources</b></h4>
+	</div>
+	<span class="separator-line separator-line-right"></span>
+</div>
 
 <div class="row-wrapper">
 <div class="row-container container row-full-width">
@@ -187,6 +187,163 @@
 	</div>
 	</div>
 	</div>
+
+</div>
+</div>
+</div>
+
+
+<div class="content-element column-sm-12 separator">
+							<span class="separator-line separator-line-left"></span>
+							<div class="separator-content">
+								<h4 class="separator-title"><b>HIV SELF TEST LAUNCH</b></h4>
+							</div>
+							<span class="separator-line separator-line-right"></span>
+						</div>
+
+
+				<div class="row-wrapper">
+					<div class="row-container container">
+						<div class="row">
+
+
+
+							<div class="column col-sm-4">
+								<div class="column-inner">
+									<div class="column-content">
+										<div class="content-element video-wrapper">
+											<div class="iframe-wrapper iframe-video">
+												<iframe width="560" height="315" src="https://www.youtube.com/embed/9I0_e1Igg2U" frameborder="0" allowfullscreen></iframe>
+											</div>
+										</div>
+										<div><h3>NASCOP Launches HIV Prevention Drug</h3></div>
+									</div>
+								</div>
+							</div>
+
+
+							<div class="column col-sm-4">
+								<div class="column-inner">
+									<div class="column-content">
+										<div class="content-element video-wrapper">
+											<div class="iframe-wrapper iframe-video">
+												<iframe width="560" height="315" src="https://www.youtube.com/embed/H9y4uImw5V8" frameborder="0" allowfullscreen></iframe>
+											</div>
+										</div>
+										<div><h3>HIV Self Testing Discussion</h3></div>
+									</div>
+								</div>
+							</div>
+
+
+							<div class="column col-sm-4">
+								<div class="column-inner">
+									<div class="column-content">
+										<div class="content-element video-wrapper">
+											<div class="iframe-wrapper iframe-video">
+												<iframe width="560" height="315" src="https://www.youtube.com/embed/Z4o90zghY5A" frameborder="0" allowfullscreen></iframe>
+											</div>
+										</div>
+										<div><h3>WAR on HIV at Citizen TV</h3></div>
+									</div>
+								</div>
+							</div>
+
+
+						</div>
+					</div>
+				</div>
+
+
+				<div class="content-element column-sm-12 separator">
+							<span class="separator-line separator-line-left"></span>
+							<div class="separator-content">
+								<h4 class="separator-title"><b>OTHER LAUNCH LINKS</b></h4>
+							</div>
+							<span class="separator-line separator-line-right"></span>
+						</div>
+
+
+<div class="row-wrapper">
+<div class="row-container container row-full-width">
+<div class="row columns-medium-margins">
+	<div class="column col-sm-3">
+	<div class="column-inner">
+	<div class="column-content">
+
+	<a href="http://www.nation.co.ke/news/Govt-launches-two-approaches-to-fight-HIV-Aids/1056-3914614-8p6ubc/">
+	<div class="content-element team-wrapper team-align-left team-boxed">
+	<div class="image-thumbnail" style="padding-bottom:100%">
+	<img src="<?= @$this->config->item('assets_url'); ?>img/resources/govt_launch.PNG" class="preload-image team-image lazy-load-img is-loaded is-ready" alt="Government Launch" data-src="data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" class="viba-portfolio-thumbnail preload-image lazy-load-img">
+	</div>
+	<div class="team-info">
+	<h5 class="team-name">Government Launches Two Approaches</h5></a>
+	<!-- <span class="team-desc">Designer</span>
+	<p>Maecenas sed diam eget risus varius blandit sit amet non magna. Mattis purus sit amet fermentum.</p>
+	<div class="team-icons">
+	<a href="https://facebook.com" class="team-icon apalodi-icon-facebook"></a>
+	<a href="https://twitter.com" class="team-icon apalodi-icon-twitter"></a>
+	</div> -->
+	</div>
+	</div>
+	</div>
+	</div>
+	</div>
+
+	<div class="column col-sm-3">
+	<div class="column-inner">
+	<div class="column-content">
+
+	<a href="https://www.standardmedia.co.ke/health/article/2001238625/hiv-home-test-kits-now-in-kenya">
+	<div class="content-element team-wrapper team-align-left team-boxed">
+	<div class="image-thumbnail" style="padding-bottom:100%">
+	<img src="<?= @$this->config->item('assets_url'); ?>img/resources/standard_home_kits.png" class="preload-image team-image lazy-load-img is-loaded is-ready" alt="Home Test Kits" data-src="data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" class="viba-portfolio-thumbnail preload-image lazy-load-img">
+	</div>
+	<div class="team-info">
+	<h5 class="team-name">Stardand Digital Home Test Kits</h5></a>
+	
+	</div>
+	</div>
+	</div>
+	</div>
+	</div>
+
+	<div class="column col-sm-3">
+	<div class="column-inner">
+	<div class="column-content">
+
+	<a href="http://www.unaids.org/en/resources/presscentre/featurestories/2017/may/20170505_kenya">
+	<div class="content-element team-wrapper team-align-left team-boxed">
+	<div class="image-thumbnail" style="padding-bottom:100%">
+	<img src="<?= @$this->config->item('assets_url'); ?>img/resources/unaids_self_test.png" class="preload-image team-image lazy-load-img is-loaded is-ready" alt="Home Test Kits" data-src="data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" class="viba-portfolio-thumbnail preload-image lazy-load-img">
+	</div>
+	<div class="team-info">
+	<h5 class="team-name">Kenya Launches Self Test Kits</h5></a>
+	
+	</div>
+	</div>
+	</div>
+	</div>
+	</div>
+
+	<!-- <div class="column col-sm-3">
+	<div class="column-inner">
+	<div class="column-content">
+	<a href="http://nacc.or.ke/">
+	<div class="content-element team-wrapper team-align-left team-boxed">
+	<div class="image-thumbnail" style="padding-bottom:100%">
+	<img style="margin-top:40%;height:30%;" src="<?= @$this->config->item('assets_url'); ?>img/resources/unaids_self_test.PNG" class="preload-image team-image lazy-load-img is-loaded is-ready" alt="Nacc" data-src="data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" class="viba-portfolio-thumbnail preload-image lazy-load-img">
+	</div>
+	<div class="team-info">
+	<a href="http://nacc.or.ke/"><h5 class="team-name">Kenya Luanches self test kits</h5></a>
+	</div>
+	</div>
+	</div>
+	</div>
+	</div> -->
+
+
+
 
 </div>
 </div>
